7bf8b597c67ac8644aa26cc94945034eb91ac954
N1khilM <nikhilkingh008@gmail.com>
2025-05-07 12:15:00 +0530

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,21 @@ export const metadata: Metadata = {
   title: "NikhilPortfolio",
   description: "Created by Nikhil Mugali",
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    icon: "/favicon.ico", // Default favicon for browsers
+    shortcut: "/favicon.ico", // Shortcut icon (optional, can be same as icon)
+    apple: "/apple-touch-icon.png", // For iOS devices (e.g., 180x180 PNG)
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png", // Additional favicon for other use cases
+      },
+      {
+        rel: "icon",
+        url: "/favicon-16x16.png",
+      },
+    ],
   },
-  // themeColor: "#000000",
+  manifest: "/site.webmanifest", // Add this line
 };
 
 export default function RootLayout({
